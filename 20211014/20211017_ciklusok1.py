371b3349ebbada4cea248bc61b01221eb680165a
Szász Benjámin István <szasz.benjamin@sulinet.csmszki>
2021-10-14 09:39:38 +0200

    órai munka

---
+++ 20211014/20211017_ciklusok1.py
@@ -0,0 +1,41 @@
+def feladat1():
+    for i in range(1,21):
+        print(i,end=" ")
+    print()
+    for i in range(1,21):
+        print(i)
+
+def feladat2():
+    for i in range(15,93):
+        print(i,end=" ")
+
+def feladat3():
+    for i in range(1,31):
+        if i%2==0:
+            print(i,end=" ")
+
+def feladat4(): #hf
+    print()
+def feladat5(): #hf
+    print()
+def feladat9(): #hf
+    print()
+
+def feladat10():
+    for i in range(100,-101,-1):
+        if i%9==0:
+            print(i,end=" ")
+
+def feladat8():
+    for i in range(30,101):
+        if i+9:
+            print(i,end=" ")
+
+
+
+#itt kezdődik a függvény
+#feladat1()
+#feladat2()
+#feladat3()
+#feladat10()
+feladat8()
