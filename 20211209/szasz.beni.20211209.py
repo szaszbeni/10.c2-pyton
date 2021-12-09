562e4dff06a6a39a70f2a6e60e397c249c692954
Szász Benjámin István <szasz.benjamin@sulinet.csmszki>
2021-12-09 09:40:56 +0100

    gyakorlas

---
+++ 20211209/szasz.beni.20211209.py
@@ -0,0 +1,51 @@
+from math import *
+import random
+def feladat1():
+    a = "szöveg"
+
+def feladat2():
+    b = 5
+
+def feladat3():
+    j = 55
+
+def feladat4():
+    c = 5,12
+
+def feladat5():
+    d = 5+5
+
+def feladat6():
+    e=input("szo")
+
+def feladat7():
+    f=input("Ey mondat.")
+
+def feladat8():
+    g=input("Egy karakter.")
+
+def feladat9():
+    h=int(input("Egy egész szám."))
+
+def feladat10():
+    i = 5
+    print(i)
+    print(i*2)
+    print(i*math.pi)
+    print(3**i)
+    print(sqrt(i))
+
+def feladat11():
+    k=random.randint(10,50)
+
+feladat1()
+feladat2()
+feladat3()
+feladat4()
+feladat5()
+feladat6()
+feladat7()
+feladat8()
+feladat9()
+feladat10()
+feladat11()
